bcd69e9f6a6971f654fd1a2d45ced0c7948af12e
StarHub190 <muhammadzidanpriadi843@gmail.com>
2026-07-13 13:48:46 +0700

    set boilerplate AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import LoginForm from "./components/auth/loginform";
 import RegistForm from "./components/auth/registform";
+import AuthProvider  from "./components/auth/AuthContext";
 
 function App() {
   return (
+    <AuthProvider>
     <div className="relative flex min-h-screen items-center justify-center overflow-hidden bg-slate-50 px-4 py-10">
       <div className="relative w-full max-w-md">
         <LoginForm />
       </div>
     </div>
+    </AuthProvider>
   );
 }
 
